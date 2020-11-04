3d97b4734a00682182f99a0509e89925d01f6afc
alex42q <alexander@zorbadakis.com>
2020-11-04 17:20:17 +0200

    commit

--- bmi calculator/js/calculate.js
+++ bmi calculator/js/calculate.js
@@ -9,13 +9,13 @@ function calc(){
     if(bmi<=18.5){
         var apo = result.innerText = "underweight";
     }else if(bmi>= 18.5 && bmi<=24.9){
-        var apo =result.innerText = "normal weight";
+        var apo =result.innerText = "normal";
     }else if(bmi>=25 && bmi<=29.9){
         var apo =result.innerText = "overweight";
     }else if(bmi>=30){
         var apo =result.innerText = "obesity";
     }
-    result.innerText ="your BMI is "+ bmi + ", " + " and you are " + apo;
+    result.innerText ="your BMI is "+ Math.floor(bmi) + ", " + " and your weight is " + apo;
     console.log(bmi)
 }
 
