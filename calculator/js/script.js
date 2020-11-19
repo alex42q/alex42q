8b16617b02c6be493cb93dd46714dbe1248120fe
alex42q <alexander@zorbadakis.com>
2020-11-19 16:58:10 +0200

    commit

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,9 +1,26 @@
-
 function calculator(){
-    var n9 = document.getElementById("n9").addEventListener("click", calculator)
-    var screen = document.getElementById("screen");
-    if(n9.onclick){
-        screen.innerText = n9.value;
-    }
+    let screen = document.getElementById("screen");
+    let buttons = document.querySelectorAll(".btn");
+    let clear = document.getElementById("btn-clear");
+    let buttonEqual = document.getElementById("btn-equal");
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+          let value = e.target.dataset.num;
+          screen.value += value;
+        })
+});
+
+    buttonEqual.addEventListener("click", function(e){
+        plus()
+        screen.value = result;
+    })
+
 
 }
+
+function plus(num1,num2){
+    let result = num1 + num2;
+}
+
+calculator()
