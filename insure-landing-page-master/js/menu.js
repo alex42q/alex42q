514933dbd3d19325e2225d782b9b2daa83d39f1c
alex42q <alexander@zorbadakis.com>
2020-11-20 15:17:34 +0200

    commit

--- insure-landing-page-master/js/menu.js
+++ insure-landing-page-master/js/menu.js
@@ -1,10 +1,24 @@
 const menu = document.getElementById("menu")
 
-menu.addEventListener("click", function(){
-    if(menu.style.display = "block"){
-        alert("hes")
-    }else if(menu.style.display = "block"){
-        alert()
-    }
-});
+
+
+function menuFunc(){
+  const nav = document.getElementById("nav-right-menu");
+  const ul = document.getElementById("toggle-ul")
+  const links = document.querySelectorAll(".nav__column-right ul li a")
+  nav.classList.toggle("nav-right-menu")
+ menu.classList.toggle("toggle-nav")
+ ul.classList.toggle("toggle-ul")
+  links.forEach(function(links){
+    links.classList.toggle("li-toggle")
+  })
+
+
+}
+
+
+
+
+
+menu.addEventListener("click", menuFunc)
 
